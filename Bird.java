d77255e2d7ee1211beff3c815afd9d4b7063ef97
lamtrg <76791917+dimecity@users.noreply.github.com>
2021-01-11 12:57:50 -0600

    Add files via upload

---
+++ Bird.java
@@ -0,0 +1,44 @@
+import java.awt.*;
+import java.util.*;
+
+
+public class Bird extends Rectangle {	
+
+	private static final long serialVersionUID = 1L;
+	private int cord = 6; 
+	public boolean isPressed = false;
+	private ArrayList<Rectangle> tubes; 
+	
+	public Bird(int x, int y, ArrayList<Rectangle> tubes) {
+		setBounds(x, y, 32, 32);
+		this.tubes = tubes;
+	}
+	
+	public void update() {
+		if (isPressed == true) {
+			y -= cord;
+		}
+		else 
+			y += cord;
+		
+		for (int i = 0; i < tubes.size(); i++) {
+			if(this.intersects(tubes.get(i))) {
+					Flappy.room = new Room(60);
+					tubes = Flappy.room.tubes;
+					y= Flappy.SCREEN_HEIGHT / 2;
+					Flappy.score = 0;
+					break;
+			}
+			if (y >= Flappy.SCREEN_HEIGHT) {
+				Flappy.room = new Room(60);
+				tubes = Flappy.room.tubes;
+				y= Flappy.SCREEN_HEIGHT / 2;
+			}
+		}
+	}
+	
+	public void render(Graphics g) {
+		g.setColor(Color.gray);
+		g.fillOval(x, y, width, height);		
+	}
+}
---
+++ Flappy.java
@@ -0,0 +1,121 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferStrategy;
+import javax.swing.JFrame;
+
+
+public class Flappy extends Canvas implements Runnable, KeyListener{
+	
+	private static final long serialVersionUID = 1L;
+	static final int SCREEN_WIDTH = 700;
+	static final int SCREEN_HEIGHT = 480;
+	private boolean running = false;
+	private Thread thread;
+	public static double score = 0.0;
+	public static Room room;
+	public Bird bird;
+	
+	public Flappy() {
+		Dimension d = new Dimension(Flappy.SCREEN_WIDTH, Flappy.SCREEN_HEIGHT);
+		setPreferredSize(d);
+		addKeyListener(this);
+		room = new Room(60);
+		bird = new Bird(100, Flappy.SCREEN_HEIGHT/2, room.tubes);
+	}
+		
+	public synchronized void startGame() {
+		if(running) return;
+		running = true;
+		thread = new Thread(this);
+		thread.start();
+	}
+	
+	public synchronized void gameOver() {
+		if(!running) return;
+		running = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Flappy Bird");
+		Flappy flappy = new Flappy();
+		frame.add(flappy);
+		frame.setResizable(false);
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);	
+		frame.setVisible(true);
+		
+		flappy.startGame();
+	}	
+
+	
+	@Override
+	public void run() {	
+		long lastTime = System.nanoTime();
+		double nanoSecond = 1000000000 / 60;
+		double delta = 0;
+		while(running) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / nanoSecond;
+			lastTime = now;
+			while (delta >= 2) {
+				update();
+				render();
+				delta =  0.8;
+			}			
+		}
+		gameOver();
+	}
+	
+	private void render() {
+		BufferStrategy bs = getBufferStrategy();
+		if(bs == null) {
+			createBufferStrategy(3);
+			return;
+		}
+		Graphics g = bs.getDrawGraphics();
+		g.setColor(Color.cyan);
+		g.fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+		room.render(g);
+		bird.render(g);
+		g.setColor(Color.black);
+		g.setFont(new Font("Calibri",Font.BOLD,19));
+		g.drawString("Score: " + (int)score, 10, 20);
+		g.dispose();
+		bs.show();
+	}
+	
+	private void update() {
+		bird.update();
+		room.update();
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_UP) {
+			bird.isPressed = true;
+		}	
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_UP) {
+			bird.isPressed = false;
+		}	
+		
+	}
+	
+	
+	
+}	
+
