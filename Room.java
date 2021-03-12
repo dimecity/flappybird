6c2e9f5a16cc31ee905776f0573ffaea5cf753dc
lamtrg <76791917+dimecity@users.noreply.github.com>
2021-03-12 15:11:20 -0600

    Update Room.java

--- Room.java
+++ Room.java
@@ -6,7 +6,7 @@ public class Room {
 	public ArrayList<Rectangle> tubes;
 	private int time;
 	private int currentTime = 0;
-	private int density = 5;
+	private int density = 5;	//how many tubes in the frame 
 	private Random random;
 	private final int SPACE_TUBES = 150; 
 	private final int WIDTH_TUBES = 64;
@@ -18,32 +18,32 @@ public class Room {
 	}
 	
 	public void update() {
-		currentTime++;
+		currentTime++;	//like density, kinda, it currenttime reaches time in the constructor then a new tube will be spawned 
 		if(currentTime == time) {
 			currentTime = 0;
 			
-			int height1 = random.nextInt(Flappy.SCREEN_HEIGHT / 2);
+			int height1 = random.nextInt(Flappy.SCREEN_HEIGHT / 2);	//create random y cordinate of the tubes  each time 
 			
-			int y2 = height1 + SPACE_TUBES; 
-			int height2 = Flappy.SCREEN_HEIGHT - y2;
+			int y2 = height1 + SPACE_TUBES; 				//calculate the dimension of the other opposite tubes
+			int height2 = Flappy.SCREEN_HEIGHT - y2;		
 		
-			tubes.add(new Rectangle(Flappy.SCREEN_WIDTH, 0, WIDTH_TUBES,height1));
+			tubes.add(new Rectangle(Flappy.SCREEN_WIDTH, 0, WIDTH_TUBES,height1)); //add them to the list 
 			tubes.add(new Rectangle(Flappy.SCREEN_WIDTH, y2, WIDTH_TUBES, height2));
 		}
 		
 		for(int i = 0; i <  tubes.size(); i++) {
-			Rectangle rect = tubes.get(i);
-			rect.x -= density;		
+			Rectangle rect = tubes.get(i); 	//new rectangle object 
+			rect.x -= density;		//the spaces between the horizontal tubes 
 			
-			if(rect.x + rect.width <= 0) {
+			if(rect.x + rect.width <= 0) {	//remove the tubes so that the list won't be infinite 
 				tubes.remove(i--);
-				Flappy.score += 0.5;
+				Flappy.score += 0.5;	//add score each time the bird passes the tubes, 0.5 but pass to int so it'll be 1
 				continue;
 			}
 		}
 	}
 	
-	public void render(Graphics g) {
+	public void render(Graphics g) {	//Draw the tubes 
 		g.setColor(Color.green);
 		
 		for(int i = 0; i < tubes.size(); i++) {
